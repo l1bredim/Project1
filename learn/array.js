5aea823520fa7cb76b38dc759cf3699ea070bd84
l1bredim <l1bredim@ukr.net>
2020-08-08 22:55:35 +0300

    array-edited

--- learn/array.js
+++ learn/array.js
@@ -1,11 +1,10 @@
-const cars = ["Mazda", "Ford", "BMW", "Mersedes"];
-const people = [
-  { name: "Viktoria", budget: 4200 },
-  { name: "Bodik", budget: 6200 },
-  { name: "Misha", budget: 1500 },
-];
-
-// const fib = [1, 1, 2, 3, 5, 8, 13];
+// const cars = ["Mazda", "Ford", "BMW", "Mersedes"];
+// // const people = [
+// //   { name: "Viktoria", budget: 4200 },
+// //   { name: "Bodik", budget: 6200 },
+// //   { name: "Misha", budget: 1500 },
+// ];
+const fib = [1, 1, 2, 3, 5, 8, 13];
 
 // cars.push("Remo");
 // console.log(cars);
@@ -45,4 +44,35 @@ const people = [
 // const person = people.find((person) => person.budget === 1500);
 // console.log(person);
 
-console.log(cars.includes("Ford"));
+// console.log(cars.includes("Ford"));
+
+// const upperCaseCars = cars.map((car) => {
+//   return car.toUpperCase();
+// });
+// console.log(upperCaseCars);
+
+// const sqrt = (num) => Math.sqrt(num);
+// const pow2Fib = fib.map(fib2).map(sqrt);
+//
+
+// const fib2 = (num) => num ** 2;
+// const pow2Fib = fib.map(fib2);
+// const filteredNumbers = pow2Fib.filter((num) => {
+//   return num > 20;
+// });
+// console.log(filteredNumbers);
+
+const people = [
+  { name: "Viktoria", budget: 4200 },
+  { name: "Bodik", budget: 6200 },
+  { name: "Misha", budget: 1500 },
+];
+
+const allBudget = people
+  .filter((person) => person.budget > 2000)
+  .reduce((acc, person) => {
+    acc += person.budget;
+    return acc;
+  }, 0);
+
+console.log(allBudget);
