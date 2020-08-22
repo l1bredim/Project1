6c7e238c8d7d1401ca002323a90305253afe3202
Mikhailo Kalonov <l1bredim@ukr.net>
2020-08-22 14:08:56 +0300

    my-bad-2task-null-results

--- learn/task2/script.js
+++ learn/task2/script.js
@@ -1,18 +1,40 @@
 function matrixRotate(rotate, direction, matrix) {
 
 }
+//долго работает!!!!
+// function fib(n){
+//     if (n===1||n===2) return 1;
+//     else return fib(n-2)+fib(n-1);		
+//     }
 
-
-function matrix() {
-    let arr = [[],[]];
-    for (let i = 0; i < 10; i++) {
-        for (let j = 0; j < 10; j++) {
-            arr[i]=+ i;
-            arr[j]=+j;
-        }
-     
-      console.log(arr[i,j]);
+function fib(x){
+    let a = 1, b = 1;
+    let c = a + b;
+    for (let i = 3; i <=x; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
     }
-    
+    return b;
 }
+
+let arr = [];
+function matrix(n,m) {
+let a = 1; 
+let b = 1; 
+let c = a + b;
+    for (i = 0; i < n; i++) {
+            arr[i] = [];
+    for (j = 0; j < m; j++) {
+        let c = a + b;
+        a = b;
+        b = c;
+        arr[i][j] = j;
+        }
+        return b;
+    }
+    console.log(arr)
+}
+
+
 
