dabbe6c3c471dfd7b934746a253fc12ea39776ae
l1bredim <l1bredim@ukr.net>
2020-08-25 23:06:55 +0300

    tast2-50persent-complete

--- learn/task2/script.js
+++ learn/task2/script.js
@@ -1,40 +1,38 @@
-function matrixRotate(rotate, direction, matrix) {
-
-}
+function matrixRotate(rotate, direction, matrix) {}
 //долго работает!!!!
 // function fib(n){
 //     if (n===1||n===2) return 1;
-//     else return fib(n-2)+fib(n-1);		
+//     else return fib(n-2)+fib(n-1);
 //     }
 
-function fib(x){
-    let a = 1, b = 1;
+function fib(x) {
+  if (x === 0) {
+    return 0;
+  } else {
+    let a = 1,
+      b = 1;
     let c = a + b;
-    for (let i = 3; i <=x; i++) {
-        let c = a + b;
-        a = b;
-        b = c;
+
+    for (let i = 3; i <= x; i++) {
+      let c = a + b;
+      a = b;
+      b = c;
     }
+
     return b;
+  }
 }
 
 let arr = [];
-function matrix(n,m) {
-let a = 1; 
-let b = 1; 
-let c = a + b;
-    for (i = 0; i < n; i++) {
-            arr[i] = [];
-    for (j = 0; j < m; j++) {
-        let c = a + b;
-        a = b;
-        b = c;
-        arr[i][j] = j;
-        }
-        return b;
+
+function matrix(n, m) {
+  let number = 0;
+  for (i = 0; i < m; i++) {
+    arr[i] = [];
+    for (j = 0; j < n; j++) {
+      arr[i][j] = fib(number);
+      number++;
     }
-    console.log(arr)
+  }
+  console.log(arr);
 }
-
-
-
