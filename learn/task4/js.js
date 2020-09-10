74724151de97a2205089e17591ceceddf279a21d
l1bredim <l1bredim@ukr.net>
2020-09-11 00:04:53 +0300

    task4

--- learn/task4/js.js
+++ learn/task4/js.js
@@ -2,12 +2,48 @@
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
-const number = getRandomInt;
-let arr = [];
+const n = 100;
+const arr = Array.from({ length: n }, () => getRandomInt(100));
 
-for (i = 0; i < 100; i++) {
-  arr[i] = number(i);
+function filter(array, callback) {
+  const newArray = [];
+  for (i = 0; i < array.length; i++) {
+    const item = array[i];
+    const call = callback(item);
+    if (call) {
+      newArray.push(item);
+    }
+  }
+  return newArray;
+}
+function map(array, callback) {
+  const newArr = [];
+  for (i = 0; i < array.length; i++) {
+    const item = array[i];
+    const call = callback(item);
+    newArr.push(call);
+  }
+  return newArr;
+}
+const filterArr = filter(
+  arr,
+  (item) => getRandomInt(item) <= getRandomInt(item / 2)
+);
+
+function li(item) {
+  return `<li>${item}</li>`;
 }
 
-const n = 100;
-const newArray = Array.from({ length: n }, (n) => new Array(n).map(number));
+const mapArr = map(filterArr, li);
+
+function forEach(array, callback) {
+  for (i = 0; i < array.length; i++) {
+    const item = array[i];
+    const oneCall = callback(item);
+    console.log(oneCall);
+  }
+}
+
+console.log(arr);
+console.log(filterArr);
+console.log(mapArr);
