ef49e2535e65dbe137c5558b38e37989f202f682
l1bredim <l1bredim@ukr.net>
2020-09-13 00:20:58 +0300

    task3

--- learn/task3/script.js
+++ learn/task3/script.js
@@ -1,12 +1,9 @@
-function map(array, cb) {
-  const newArray = [];
-
-  for (let i = 0; i < array.length; i++) {
-    const mapped = cb(arr[i]);
-    newArray.push(mapped);
-  }
-
-  return newArray;
+const modal = {
+  title: 'Modal Window',
+  closable: true;
+  content: `
+  <h4>Modal is Work</h4>
+  <p>test1, test2, test3, test4</p>
+  `,
+  width: '500px';
 }
-
-map(10);
