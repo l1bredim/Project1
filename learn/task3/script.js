1db69825ec492a175fba8846462c9126aa7fd10c
l1bredim <l1bredim@ukr.net>
2020-09-16 23:44:35 +0300

    task3

--- learn/task3/script.js
+++ learn/task3/script.js
@@ -1,9 +1,70 @@
-const modal = {
-  title: 'Modal Window',
-  closable: true;
-  content: `
-  <h4>Modal is Work</h4>
-  <p>test1, test2, test3, test4</p>
-  `,
-  width: '500px';
+const my = {};
+window.my = my;
+function createModal(options) {
+  const DEFAULT_WIDTH = "600PX";
+  const modal = document.createElement("div");
+  modal.classList.add("modal");
+  modal.insertAdjacentHTML(
+    "afterbegin",
+    `
+  <div class="modal-overlay" data-close="true">
+    <div class="modal-window" style='width: ${options.width || DEFAULT_WIDTH}'>
+      <div class="modal-header">
+        <span class="modal-title">${options.title || "Всплывающее окно"}</span>
+        ${
+          options.closable
+            ? `<span class="modal-close" data-close="true" >&times;</span>`
+            : ""
+        }
+        
+      </div>
+      <div class="modal-body">
+        ${options.content || ""}
+      </div>
+      <div class="modal-footer">
+        <button id="myButton">OK</button>
+        <button id="close" data-close="true" >Cancel</button>
+      </div>
+    </div>
+  </div>
+`
+  );
+  document.body.appendChild(modal);
+  return modal;
 }
+my.modal = function (options) {
+  const ANIMATION_SPEED = 200;
+  let closing = false;
+
+  const modal = {
+    open() {
+      !closing && newModal.classList.add("open");
+    },
+    close() {
+      closing = true;
+      newModal.classList.remove("open");
+      newModal.classList.add("hide");
+      setTimeout(() => {
+        newModal.classList.remove("hide");
+        closing = false;
+      }, ANIMATION_SPEED);
+    },
+  };
+  const newModal = createModal(options);
+  newModal.addEventListener("click", (event) => {
+    if (event.target.dataset.close) {
+      modal.close();
+    }
+  });
+  return modal;
+};
+
+const mod = my.modal({
+  title: "Булытки пиваса!!!",
+  closable: true,
+  content: `
+  <p>Вывод 1 инпута</p>
+  <p>Вывод 2 инпута</p>
+  <p>Вывод 3 инпута</p>`,
+  width: "400px",
+});
--- learn/task4/js.js
+++ learn/task4/js.js
@@ -3,7 +3,7 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 const n = 50;
-const max = 20;
+const max = 100;
 const arr = Array.from({ length: n }, () => getRandomInt(max));
 
 const filterArr = filter(arr, (item) => item <= max / 2);
@@ -16,25 +16,48 @@ const mapArr = map(filterArr, function (item) {
     li.classList.add("li-red");
   }
   li.textContent = item;
-  console.log(li.classList);
   return li;
 });
 
-const forEachArr = forEach(mapArr, function (item) {
-  const ul = document.querySelector("ul");
-
-  ul.appendChild(item);
-});
+// });
+// });
 // for (i = 0; i < 10; i++) {
 //   let li = document.createElement("li");
 //   li.append(getRandomInt(100));
 //   console.log(li);
 // }
-let call = document.querySelector("call");
-console.log(arr);
-console.log(filterArr);
-console.log(mapArr);
-console.log(forEachArr);
+// let call = document.querySelector("call");
+// // console.log(arr);
+// // console.log(filterArr);
+// // console.log(mapArr);
+// console.log(forEachArr);
 
-let amount = filterArr.reduce((total, item) => total + item, 0);
-console.log(amount);
+// let amount = filterArr.reduce((total, item) => total + item, 0);
+// console.log(amount);
+
+function createClass(item) {
+  const li = document.createElement("li");
+  const ul = document.querySelector("ul");
+  if (item % 2 === 0) {
+    li.classList.add("li-green");
+  } else {
+    li.classList.add("li-red");
+  }
+  li.textContent = item;
+  return li;
+}
+
+const final = arr.reduce((acc, current) => {
+  if (current <= max / 2) {
+    const li = createClass(current);
+    acc.push(li);
+  }
+  return acc;
+}, []);
+
+const forEachArr = forEach(final, function (item) {
+  const ul = document.querySelector("ul");
+  ul.appendChild(item);
+});
+forEachArr;
+console.log(final);
