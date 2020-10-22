90cb1b3af2fc96f9e4b850b98ac61c6332f589ee
l1bredim <l1bredim@ukr.net>
2020-10-22 07:31:14 +0300

    task9

--- learn/task9/script.js
+++ learn/task9/script.js
@@ -1,21 +1,14 @@
-const array = [1,2];
-const target = 3;
+const array = [1,4,0,4,10,1,2];
+const target = 2;
 
-function targer(array) {
- let result = []; 
-for(let i=0; i<array.length; i++) {
-  let indexNext = i+1;
-  let indexPrev = i-1;
-  const itemNext = array[i+1];
-  const itemPrev = array[i-1];
-  if ((itemPrev + itemNext)==target ){
-    console.log('есть такая пара');
-    console.log(itemPrev);
-    console.log(itemNext);
-    console.log(indexPrev);
-    console.log(indexNext);    
-    result.push(indexPrev,indexNext);
-  }
- }
+function toObject(array) {
+let result = []; 
+label: for(let i=0; i<array.length-1; i++) 
+       for (let j=i+1; j<array.length; j++) 
+    if ((array[i] + array[j])==target ) {
+      result.push(i,j)
+      break label;
+    } 
+
  return result;
 }
