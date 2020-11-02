b858f31b047f68773ba0ec0803a7185566a89058
l1bredim <l1bredim@ukr.net>
2020-11-02 22:54:39 +0200

    clock

---
+++ learn/clock/script.js
@@ -0,0 +1,26 @@
+const months = document.querySelector('.month');
+const hours = document.querySelector('.hours');
+const minutes = document.querySelector('.minutes');
+const seconds = document.querySelector('.seconds');
+
+function clock() {
+    const month = new Date().getMonth() + 1;
+    const hour = new Date().getHours();
+    const min = new Date().getMinutes();
+    const sec = new Date().getSeconds();
+
+   if (month < 10) {
+       months.innerText = '0' + month;
+    } else (months.innerText =  month);
+    if (hour < 10) {
+        hours.innerText = '0' + hour;
+     } else (hours.innerText =  hour);
+     if (min < 10) {
+        minutes.innerText = '0' + min;
+     } else (minutes.innerText =  min);
+     if (sec < 10) {
+        seconds.innerText = '0' + sec;
+     } else (seconds.innerText =  sec);
+}
+clock();
+setInterval(clock,1000);
