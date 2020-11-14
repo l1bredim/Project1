a112092f279da43ce9e885916c9771e870f709a2
l1bredim <l1bredim@ukr.net>
2020-11-15 00:21:07 +0200

    slider

---
+++ learn/task11/script.js
@@ -0,0 +1,11 @@
+const btnNext = document.querySelector('.item_next').onclick = nextImage;
+const btnPrev = document.querySelector('item_prev');
+const sliderItem = document.querySelector('.slider__item');
+
+function nextImage() {
+  
+}
+
+function prevImage(){
+
+}
