b0560a8f09fddfd8424b2e30b8b13f58bb69dbd5
l1bredim <l1bredim@ukr.net>
2020-11-24 23:30:06 +0200

    slider

--- learn/task11/script.js
+++ learn/task11/script.js
@@ -1,11 +1,39 @@
-const btnNext = document.querySelector('.item_next').onclick = nextImage;
-const btnPrev = document.querySelector('item_prev');
-const sliderItem = document.querySelector('.slider__item');
+const btnNext = document.querySelector('.item_next'),
+      btnPrev = document.querySelector('.item_prev'),
+      slides = document.querySelectorAll('.slide');
 
-function nextImage() {
-  
+let index = 0;
+
+function activeSlide(item) {
+  for(slide of slides) {
+    slide.classList.remove('active')
+  }
+  slides[item].classList.add('active');
 }
 
-function prevImage(){
-
+function nextSlide() {
+  if (index == slides.length-1) {
+    index = 0;
+    activeSlide(index);
+  } else {
+    index++
+    activeSlide(index);
+  }
 }
+
+function prevSlide() {
+  if (index == 0) {
+    index = slides.length-1;
+    activeSlide(index);
+  } else {
+    index--
+    activeSlide(index);
+  }
+}
+
+
+btnNext.addEventListener('click', nextSlide);
+btnPrev.addEventListener('click', prevSlide);
+
+
+setInterval(nextSlide,2000);
