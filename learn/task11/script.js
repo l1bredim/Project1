50a5c4239948a672c5bd594529b921ffaa4e8fec
l1bredim <l1bredim@ukr.net>
2020-11-27 00:43:32 +0200

    slider

--- learn/task11/script.js
+++ learn/task11/script.js
@@ -1,8 +1,17 @@
 const btnNext = document.querySelector('.item_next'),
       btnPrev = document.querySelector('.item_prev'),
       slides = document.querySelectorAll('.slide');
+      points = document.querySelectorAll('.point');
+
 
 let index = 0;
+
+function activePoint(item) {
+  for(point of points) {
+    point.classList.remove('active')
+  }
+  points[item].classList.add('active');
+}
 
 function activeSlide(item) {
   for(slide of slides) {
@@ -15,25 +24,38 @@ function nextSlide() {
   if (index == slides.length-1) {
     index = 0;
     activeSlide(index);
+    activePoint(index);
+
   } else {
     index++
     activeSlide(index);
+    activePoint(index);
+
   }
+
 }
 
 function prevSlide() {
   if (index == 0) {
     index = slides.length-1;
     activeSlide(index);
+    activePoint(index);
   } else {
     index--
+    activePoint(index);
     activeSlide(index);
   }
 }
 
-
+points.forEach((item, indexPoint)=> {
+  item.addEventListener('click', ()=> {
+    index = indexPoint;
+    activePoint(index);
+    activeSlide(index)
+  })
+  
+})
 btnNext.addEventListener('click', nextSlide);
 btnPrev.addEventListener('click', prevSlide);
 
 
-setInterval(nextSlide,2000);
