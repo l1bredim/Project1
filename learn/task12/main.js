69074c7d84ba64c899ccc4262205a4bc839629d3
l1bredim <l1bredim@ukr.net>
2021-02-16 01:05:36 +0200

    task12

--- learn/task12/main.js
+++ learn/task12/main.js
@@ -1,5 +1,15 @@
 const accordName = document.querySelector('.accord-title');
+accordName.innerHTML = '<h1>Am accord</h1>'
 
+const strings = document.querySelectorAll('.container-gn');
 function Title() {
+
+}
+
+function fingers() {
+
+}
+
+function drowAccord() {
 
 }
